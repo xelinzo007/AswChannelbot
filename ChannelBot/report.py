125b987951e19dc090ba4c259f5d97c5d90199d4
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-03-16 12:30:16 +0530

    Update report.py

--- ChannelBot/report.py
+++ ChannelBot/report.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(filters.regex(r'^Report a Problem$') | filters.command('report'))
 async def _manage(_, msg):
-    how = '**Report a Problem** \n\n'
+    how = '**Report a Problem about bot** \n\n'
     how += "If something **unexpected** happens, you can report it to us. (You can also suggest features.)\n\n"
     how += '**Steps** \n'
     how += '1) Try whatever you did again. If it shows the same unexpected thing, move to step 2 \n'
